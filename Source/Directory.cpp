647b73dc434c4e7db02daa9471c161d879457bdb
Joseph Lyons <josephlyons@Josephs-Air.PK5001Z>
2017-03-20 17:34:24 -0400

    Got the entire logic for removing empty folders done
    Need to double check to make sure this always works

--- Source/Directory.cpp
+++ Source/Directory.cpp
@@ -7,17 +7,51 @@
 //
 
 #include "Directory.hpp"
-#include "../JuceLibraryCode/JuceHeader.h"
 
 void removeEmptyFolders(const char *directoryPath)
 {
+    // Scans files and directories recursively, but skips hidden files
     DirectoryIterator dirIter (File (directoryPath), true, "*",
                                File::findFilesAndDirectories + File::ignoreHiddenFiles);
     
     File fileHolder;
     
+    // Iterate through all files and directories
     while (dirIter.next())
     {
         fileHolder = dirIter.getFile();
+        
+        if(fileHolder.isDirectory())
+        {
+            if(folderIsEmpty(fileHolder))
+            {
+                fileHolder.moveToTrash();
+            }
+        }
     }
+}
+
+bool folderIsEmpty(File fileHolder)
+{
+    // Scans files and directories non-recursively, but skips hidden files
+    // MAKE AN OPTION TO NOT DELETE FOLDERS WITH HIDDEN ITEMS?
+    DirectoryIterator dirIter (File (fileHolder.getFullPathName()), false, "*",
+                               File::findFilesAndDirectories + File::ignoreHiddenFiles);
+    
+    int fileCount = 0;
+    
+    while (dirIter.next())
+    {
+        fileHolder = dirIter.getFile();
+        
+        return false;
+    }
+    
+    if(fileCount != 0)
+    {
+        return false;
+    }
+    
+    else
+        return true;
 }
--- Source/Directory.hpp
+++ Source/Directory.hpp
@@ -9,6 +9,9 @@
 #ifndef Directory_hpp
 #define Directory_hpp
 
+#include "../JuceLibraryCode/JuceHeader.h"
+
 void removeEmptyFolders(const char *directoryPath);
+bool folderIsEmpty(File fileHolder);
 
 #endif /* Directory_hpp */
