55422b6f7e1680bd2b66744a2f6063992348c2d5
Joseph Lyons <josephlyons@Josephs-Air.PK5001Z>
2017-03-23 00:14:46 -0400

    Add class framework for Capitalize Items

---
+++ Source/CapitalizeItems.cpp
@@ -0,0 +1,9 @@
+//
+//  CapitalizeItems.cpp
+//  Folder Utility
+//
+//  Created by Joseph Lyons on 3/23/17.
+//
+//
+
+#include "CapitalizeItems.hpp"
---
+++ Source/CapitalizeItems.hpp
@@ -0,0 +1,20 @@
+//
+//  CapitalizeItems.hpp
+//  Folder Utility
+//
+//  Created by Joseph Lyons on 3/23/17.
+//
+//
+
+#ifndef CapitalizeItems_hpp
+#define CapitalizeItems_hpp
+
+class CapitalizeItems
+{
+private:
+    
+public:
+    
+};
+
+#endif /* CapitalizeItems_hpp */
