d18c3a5c93a1cbb9a3d4622a9684ce3828dd8e9b
Joseph Lyons <josephlyons@Josephs-Air.PK5001Z>
2017-03-24 00:21:30 -0400

    Added a class for analyzing files
    Will return count of file extensions
    Return all files

---
+++ Source/AnalyzeFiles.cpp
@@ -0,0 +1,9 @@
+//
+//  AnalyzeFiles.cpp
+//  Folder Utility
+//
+//  Created by Joseph Lyons on 3/24/17.
+//
+//
+
+#include "AnalyzeFiles.hpp"
---
+++ Source/AnalyzeFiles.hpp
@@ -0,0 +1,18 @@
+//
+//  AnalyzeFiles.hpp
+//  Folder Utility
+//
+//  Created by Joseph Lyons on 3/24/17.
+//
+//
+
+#ifndef AnalyzeFiles_hpp
+#define AnalyzeFiles_hpp
+
+class AnalyzeFiles
+{
+private:
+public:
+};
+
+#endif /* AnalyzeFiles_hpp */
